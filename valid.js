3ad877638afded48b2cae22cb0d4d908e1e91b98
Abhijith Babu <109859615+Abijithbabu@users.noreply.github.com>
2023-06-29 14:16:48 +0530

    script files added

---
+++ valid.js
@@ -0,0 +1,64 @@
+function validateName() {
+  var name = document.getElementById("name").value;
+  if (name.length <3 || /^[A-Za-z ]+$/.test(name)==false) {
+    document.getElementById("errName").innerHTML = "Please Enter valid Name";
+    return false;
+  }
+  else{
+    document.getElementById("errName").innerHTML = "";
+    return true;
+  }
+}
+
+
+function validateEmail() {
+  var email = document.getElementById("email").value;
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)==false ) {
+    document.getElementById("errEmail").innerHTML ="Please Enter valid Email";
+    return false;
+  }
+  else
+    document.getElementById("errEmail").innerHTML = "";
+    return true;
+}  
+
+
+function validateMsg() {
+  var message = document.getElementById("message").value;
+  if (message.length <= 15) { 
+    document.getElementById("errMsg").innerHTML = "Please Enter More Than 15 Characters";
+    return false;
+  }
+  else{
+    document.getElementById("errMsg").innerHTML = "";
+    return true;
+  }
+}
+
+
+function validate() {
+  if( validateName() && validateEmail() && validateMsg()){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+
+$("#submit-form").submit((e)=>{
+  e.preventDefault()
+  if(validate()){
+  $.ajax({
+      url:"https://script.google.com/macros/s/AKfycbwzjj_52ORk9ilMfCEC12PJ2VNPmoej1BIEYxqiSRTY_b_yyrN2AFPQvspvQxmRnNHFwQ/exec",
+      data:$("#submit-form").serialize(),
+      method:"post",
+      success:function (response){
+          alert("Form submitted successfully !!")
+          window.location.reload()
+      },
+      error:function (err){
+          alert("Something went Wrong")
+      }
+   })
+  }
+})
